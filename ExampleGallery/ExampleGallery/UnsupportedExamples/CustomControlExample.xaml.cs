cde4f4a240979d6ab80d4e65e48915d08c13a3c6
Dom Giandinoto <dgiandinoto@microsoft.com>
2021-08-30 14:05:05 -0700

    Added unsupported examples, excluded them from the project

---
+++ ExampleGallery/ExampleGallery/UnsupportedExamples/CustomControlExample.xaml.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ExampleGallery
+{
+    public sealed partial class CustomControlExample : UserControl
+    {
+        public CustomControlExample()
+        {
+            this.InitializeComponent();
+            this.InitializeThumbnail();
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width > e.NewSize.Height)
+                demoPanel.Orientation = Orientation.Horizontal;
+            else
+                demoPanel.Orientation = Orientation.Vertical;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ThumbnailGenerator.IsDrawingThumbnail)
+                return;
+
+            Pulse.Begin();
+        }
+    }
+}
