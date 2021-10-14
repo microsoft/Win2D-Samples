4b75318f24331444bd89a63e0a5071be1b24a3b7
Dom Giandinoto <dgiandinoto@microsoft.com>
2021-10-14 16:15:05 -0700

    Renamed SimpleSampleDesktop to SimpleSample

--- SimpleSampleDesktop/SimpleSample/App.xaml.cs
+++ SimpleSampleDesktop/SimpleSample/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace SimpleSampleDesktop
+namespace SimpleSample
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- SimpleSampleDesktop/SimpleSample/MainWindow.xaml.cs
+++ SimpleSampleDesktop/SimpleSample/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace SimpleSampleDesktop
+namespace SimpleSample
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
